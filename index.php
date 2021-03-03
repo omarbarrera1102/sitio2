1001013d0633fdf8637d31c2c16adb897499c10d
omarbarrera1102 <omarbarrera1102@gmail.com>
2021-03-03 14:51:44 -0600

    ADD: Agregando Sitio2 Buapo

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+echo "Nombre:";
+?>
+<br />
+<?php
+echo "Omar Barrera Santiago";
+?>
+<br />
+<?php
+echo "Datos personales";
+?>
+<br />
+<?php
+echo "Edad: 22, Carrera: Informatica.";
+?>
+<br />
+<?php
+echo "Hobbies:";
+?>
+<br />
+<?php
+echo "Me gusta ver series, ver peliculas, ver lucha libre y andar en patineta.";
+?>
+<br />
+<?php
+echo "¿A que te quieres dedicar?";
+?>
+<br />
+<?php
+echo "Me interesan mucho las bases de datos y el desarrollo, ya sea web, software o parecidos.";
+?>
+<br />
+<?php
+echo "Gracias por su atencion :D";
+?>
+<br />
